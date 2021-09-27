3d96f99062d553f052c3da612947a8674d1d2945
AlbertMiao <innocentmyl@gmail.com>
2021-09-26 21:34:06 -0400

    Yinglong: Added Controller for Agent and Camera
    Agent Controller:
    - Using RayCasting so that we can use left click to activate/deactivate the agent. Use the same mechanism for setting the navigation goal using right click
    - Control the camera by "WSAD" which will change the x and z values of the camera. "Space" and "Shift" will change the y value of the camera so we can move up and down.
    - Baking and testing that the agent is able to navigate in the environment.

--- CS428-523-B1/Assets/Scripts/AgentNavigation.cs
+++ CS428-523-B1/Assets/Scripts/AgentNavigation.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentNavigation : MonoBehaviour
 {
+    //Transform goal;
+    NavMeshAgent agent;
+    public Camera camera;
+    bool activated;  // the agent is activated if the mouse has clicked on it.
+                     // it gets deactivated if the mouse clicks again.
+    Color deactivatedColor;
+    Color activatedColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set the agent referring to self
+        agent = GetComponent<NavMeshAgent>();
+        activated = false;
+        deactivatedColor = new Color(GetComponent<Renderer>().material.color.r,
+                                    GetComponent<Renderer>().material.color.g,
+                                    GetComponent<Renderer>().material.color.b,
+                                    GetComponent<Renderer>().material.color.a);
+        activatedColor = new Color(GetComponent<Renderer>().material.color.r,//-0.4f, 
+                                    GetComponent<Renderer>().material.color.g,//-0.4f, 
+                                    GetComponent<Renderer>().material.color.b-0.6f,//-0.3f, 
+                                    GetComponent<Renderer>().material.color.a);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If we capture a mouse click, then we check if the goal is the same.
+        // If the goal has changed, we will set the goal to the new location.
+        if (activated && Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Transform objectHit = hit.transform;
+                Vector3 hitPosition = hit.point;
+                //if ((goal.position - hitPosition).sqrMagnitude >= 0.1f)
+                {
+                    // the goal position has changed, reset
+                    //goal.position = hitPosition;
+                    agent.destination = hitPosition;
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    // if the mouse selects the current player, change activation
+                    activated = (!activated);
+                    if (activated)
+                    {
+                        // change color so we know it's activated
+                        GetComponent<Renderer>().material.color = activatedColor;
+                    }
+                    else
+                    {
+                        GetComponent<Renderer>().material.color = deactivatedColor;
+
+                    }
+                }
+            }
+        }
+
+
     }
 }
---
+++ CS428-523-B1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float dz = 1;
+    float dx = 1;
+    float dy = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WSAD to move the position of the camera
+        // space to move the camera higher
+        // shift to move the camera lower
+        if (Input.GetKey("w"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[2] = current_pos[2] + dz;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[2] = current_pos[2] - dz;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("a"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[0] = current_pos[0] - dx;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("d"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[0] = current_pos[0] + dx;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("space"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[1] = current_pos[1] + dy;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("left shift"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[1] = current_pos[1] - dy;
+            this.transform.position = current_pos;
+        }
+        if (Input.GetKey("right shift"))
+        {
+            Vector3 current_pos = this.transform.position;
+            current_pos[1] = current_pos[1] - dy;
+            this.transform.position = current_pos;
+        }
+
+
+    }
+}
